9fa4706f8b6a210b61c0f06024a7e8e7710f8585
Iatkos-03 <105349434+Iatkos-03@users.noreply.github.com>
2022-09-06 14:40:56 +0200

    mise à jour de la page d'accueil du site

--- front/js/script.js
+++ front/js/script.js
@@ -1,47 +1,21 @@
 fetch("http://localhost:3000/api/products")
 .then((res) => res.json())
 .then((data) => addProducts(data))
-/*altTxt
-: 
-"Photo d'un canapé bleu, deux places"
-colors
-: 
-(3) ['Blue', 'White', 'Black']
-description
-: 
-"Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-imageUrl
-: 
-"http://localhost:3000/images/kanap01.jpeg"
-name
-: 
-"Kanap Sinopé"
-price
-: 
-1849
-_id
-: 
-"107fb5b75607497b96722bda5b504926"*/
 
 function addProducts(data) {
  data.forEach((kanap) => {
-  console.log("kanap: ", kanap)
-
-    
     const _id = kanap._id
     const imageUrl = kanap.imageUrl
     const altTxt = kanap.altTxt
     const name = kanap.name
     const description = kanap.description 
-  //const {_id, imageUrl, altTxt, name, description} = kanap
-  
-  const image = makeImage(imageUrl, altTxt)
-  const anchor = makeAnchor(_id)
-  const article = document.createElement("article")
-  const h3 = makeH3(name)
-  const p = makeParagraph(description)
-  appendElementsToArticle(article, image, h3, p)
-  appendArticleToAnchor(anchor, article)
+    const image = makeImage(imageUrl, altTxt)
+    const anchor = makeAnchor(_id)
+    const article = document.createElement("article")
+    const h3 = makeH3(name)
+    const p = makeParagraph(description)
+    appendElementsToArticle(article, image, h3, p)
+    appendArticleToAnchor(anchor, article)
 })
 }
 function appendElementsToArticle(article, image, h3, p) {
